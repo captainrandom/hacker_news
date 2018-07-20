03cf36a532d7ea50ffe83d62b8ce272866f5ad20
Dali Rojas <rojasdali@gmail.com>
2018-07-20 17:02:08 -0400

    og

---
+++ og/fetch_data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import argparse
+import pandas as pd
+
+from data.datasources import giphy
+
+
+def main(datasource, raw, file_dest):
+    if not file_dest:
+        if datasource == 'giphy':
+            items = giphy.GiphyItems()
+        else:
+            pass
+        items.print_rows()
+    else:
+        print giphy.GiphyItems()
+       # df.to_csv(file_dest)
+        print('Saving to CSV is not implemented.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Fetch data from datasources.')
+    parser.add_argument("datasource",
+                        help="Choose a datasource",
+                        action="store",
+                        choices=('giphy',))
+    parser.add_argument("--raw",
+                        help="Display raw data instead of summary rows",
+                        action="store_true",
+                        default=False,)
+    parser.add_argument("--save",
+                        help="Save data to file as a CSV",
+                        action="store",
+                        default=None,)
+    args = parser.parse_args()
+    main(args.datasource, args.raw, args.save)
